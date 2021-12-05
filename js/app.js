854178b6152c4630308bba26ac3c85c9e2413f5a
davidkiama <davidkiama98@gmail.com>
2021-12-05 21:11:09 +0300

    Refactored code. Header now comes from JS

--- js/app.js
+++ js/app.js
@@ -1,3 +1,18 @@
+const header = document.querySelector(".header");
+
+header.innerHTML = `
+  <div class="logo">
+    <h1 class="logo__name">David Kiama</h1>
+  </div>
+
+  <nav class="nav">
+    <a href="index.html" class="nav__item">About</a>
+    <a href="work.html" class="nav__item">Work</a>
+    <a href="gallery.html" class="nav__item">Gallery</a>
+    <a href="contact.html" class="nav__item">Contact</a>
+  </nav>
+`;
+
 const time = document.getElementById("time");
 
 if (time) {
